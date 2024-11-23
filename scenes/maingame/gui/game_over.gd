ef98728a4d1194729be7dea93262f7fe00d08e72
kieran <kieranloewen@live.com>
2024-11-23 17:07:15 -0600

    incorporate music

--- scenes/maingame/gui/game_over.gd
+++ scenes/maingame/gui/game_over.gd
@@ -4,6 +4,7 @@ extends Control
 @onready var background_animation_player := $background_animation_player
 @onready var text_animation_player := $color_background/CenterContainer/text_animation_player
 @onready var color_background := $color_background
+@onready var music := $"Game over music"
 
 signal game_restart()
 
@@ -18,6 +19,7 @@ func triggerMenu():
 	show();
 	background_animation_player.play("background_fade")
 	text_animation_player.play("game_over_text")
+	music.play()
 	
 
 func _on_restart_button_pressed() -> void:
