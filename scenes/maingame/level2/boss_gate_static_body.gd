a08eda606dd7dad0196d50dc8e8efbd02a647526
felix <felixkanke@gmail.com>
2024-11-24 16:14:59 -0600

    add boss gate

---
+++ scenes/maingame/level2/boss_gate_static_body.gd
@@ -0,0 +1,23 @@
+extends StaticBody2D
+
+@onready var gateSprite := $BossGateSprite
+
+var _isGateClosed = false
+
+func _ready() -> void:
+	hide()
+	collision_layer = 1
+
+func closeGate() -> void:
+	show()
+	collision_layer = 9
+	_isGateClosed = true
+	
+	var tween = create_tween()
+	tween.tween_property(gateSprite, "position", Vector2(-12,-150), .5)
+	tween.tween_property(gateSprite, "position", Vector2(-12,-200), .1)
+	tween.tween_property(gateSprite, "position", Vector2(-12,-150), .2)
+
+func _on_mini_boss_trigger_area_body_entered(body: Node2D) -> void:	
+	if !_isGateClosed:
+		closeGate()
