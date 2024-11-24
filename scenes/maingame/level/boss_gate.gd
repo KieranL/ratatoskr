f102e17337e25f89c74025375ab391717994e405
felix <felixkanke@gmail.com>
2024-11-24 13:07:29 -0600

    boss gate

--- scenes/maingame/level/boss_gate.gd
+++ scenes/maingame/level/boss_gate.gd
@@ -1,6 +1,10 @@
 class_name BossGate
 extends StaticBody2D
 
+@onready var gateRect := $BossGateRect
+
+var _isGateDropped = false
+
 func _ready() -> void:
 	hide()
 	collision_layer = 1
@@ -8,3 +12,13 @@ func _ready() -> void:
 func dropGate() -> void:
 	show()
 	collision_layer = 9
+	
+	if _isGateDropped:
+		return
+	
+	_isGateDropped = true
+	
+	var tween = create_tween()
+	tween.tween_property(gateRect, "position", Vector2(0,-224), .5)
+	tween.tween_property(gateRect, "position", Vector2(0,-264), .1)
+	tween.tween_property(gateRect, "position", Vector2(0,-224), .2)
--- scenes/maingame/level/level.gd
+++ scenes/maingame/level/level.gd
@@ -8,5 +8,4 @@ func _on_boss_zone_trigger() -> void:
 	bossGate.dropGate()
 
 func _on_boss_win_game_trigger() -> void:
-	print("win")
 	winGame.emit()
