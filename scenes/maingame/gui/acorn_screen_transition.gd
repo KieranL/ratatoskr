b831ddd72b04cc6f1ca7d2c0d62d4b4b0e1e1425
= <=>
2024-11-23 21:49:08 -0600

    make fun

---
+++ scenes/maingame/gui/acorn_screen_transition.gd
@@ -0,0 +1,22 @@
+class_name AcornScreenTransition
+extends Node2D
+
+signal finish_acorn_transition()
+
+@onready var animation_player := $AnimationPlayer
+
+var _is_playing = false;
+
+func _ready() -> void:
+	hide()
+
+
+func trigger():
+	show();
+	animation_player.play("acron_transition_animation")
+	_is_playing = true
+
+
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	if anim_name == "acron_transition_animation":
+		finish_acorn_transition.emit()
