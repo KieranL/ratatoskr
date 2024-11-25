dd4dd5820c40ae3ecb966f23c98bbccd33ac13e8
Joshua Klassen <joshuaklassen17@gmail.com>
2024-11-24 21:46:18 -0600

    Fix boss camera lock

--- scenes/maingame/level2/level_2.gd
+++ scenes/maingame/level2/level_2.gd
@@ -14,8 +14,12 @@ signal playerWin()
 @onready var _player_spawn := $PlayerSpawn
 
 func _ready() -> void:
-	_player.global_position = _player_spawn.global_position
-	_camera.global_position = _player_spawn.global_position
+	# only spawn player at spawn location if visible
+	# hide spawn location node if you want to spawn player where player is in level
+	# useful for debugging
+	if _player_spawn.visible:
+		_player.global_position = _player_spawn.global_position
+		_camera.global_position = _player_spawn.global_position
 	_camera.set_target(_player)
 	loadComplete.emit()
 
@@ -29,3 +33,11 @@ func _on_boss_gate_static_body_trigger_boss_music() -> void:
 
 func _on_spwaner_spawner_death() -> void:
 	playerWin.emit()
+
+
+func _on_mini_boss_trigger_area_body_entered(body: Node2D) -> void:
+	if body is Player:
+		# TODO, make this a node with an area then set the limits to the bounds of that area
+		const MINI_BOSS_CAMERA_LOWER_LIMIT = -2500
+		(_camera as Camera2D).limit_bottom = MINI_BOSS_CAMERA_LOWER_LIMIT
+		(_camera as Camera2D).limit_top = MINI_BOSS_CAMERA_LOWER_LIMIT - 400
